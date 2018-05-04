edb3fe9e790313f857ed3ad8d53d8fb1039ed54f
Francois Petitjean <francois.petitjean@monash.edu>
2018-05-04 16:34:57 +0200

    code optimization

--- DBA.java
+++ DBA.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (C) 2013 Francois PETITJEAN 
+ * Copyright (C) 2018 Francois PETITJEAN 
  * 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -14,7 +14,13 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  ******************************************************************************/ 
 
+import static java.lang.Math.sqrt;
+
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Random;
+
 
 /**
  * This toy class show the use of DBA.
@@ -44,23 +50,84 @@ public class DBA {
 	private static int[][] pathMatrix = new int[MAX_SEQ_LENGTH][MAX_SEQ_LENGTH];
 
 	/**
-	 * Store the length of the optimal path in each cell
-	 */
-	private static int[][] optimalPathLength = new int[MAX_SEQ_LENGTH][MAX_SEQ_LENGTH];
-
-
-	/**
-	 * Dtw Barycenter Averaging (DBA)
-	 * @param C average sequence to update
+	 * Performs the DBA averaging by first finding the median over a sample, then doing n iterations of the update
 	 * @param sequences set of sequences to average
 	 */
-	public static void DBA(double[] C, double[][] sequences) {
-
-		final ArrayList<Double>[] tupleAssociation = new ArrayList[C.length];
-		for (int i = 0; i < tupleAssociation.length; i++) {
-			tupleAssociation[i] = new ArrayList<Double>(sequences.length);
+	public static double[] performDBA(double[][] sequences) {
+		int medoidIndex = approximateMedoidIndex(sequences);
+		System.out.println(medoidIndex);
+		double[]center = Arrays.copyOf(sequences[medoidIndex], sequences[medoidIndex].length);
+		
+		for (int i = 0; i < 15; i++) {
+			center = DBAUpdate(center, sequences);
 		}
-		int nbTuplesAverageSeq, i, j, indiceRes;
+		return center;
+	}
+	
+	public static int approximateMedoidIndex(double[][] sequences) {
+		/*
+		 * we are finding the medoid, as this can take a bit of time, 
+		 * if there is more than 50 time series, we sample 50 as possible 
+		 * medoid candidates
+		 */
+		ArrayList<Integer>allIndices = new ArrayList<>();
+		for (int i = 0; i < sequences.length; i++) {
+			allIndices.add(i);
+		}
+		Collections.shuffle(allIndices);
+		ArrayList<Integer>medianIndices = new ArrayList<>();
+		for (int i = 0; i < sequences.length && i<50; i++) {
+			medianIndices.add(allIndices.get(i));
+		}
+		
+		int indexMedoid = -1;
+		double lowestSoS = Double.MAX_VALUE;
+		
+		for (int medianCandidateIndex:medianIndices) {
+			double[] possibleMedoid = sequences[medianCandidateIndex];
+			double tmpSoS = sumOfSquares(possibleMedoid, sequences);
+			if (tmpSoS < lowestSoS) {
+				indexMedoid = medianCandidateIndex;
+				lowestSoS = tmpSoS;
+			}
+		}
+		return indexMedoid;
+	}
+	
+	private static double sumOfSquares(double[]sequence,double[][]sequences) {
+		double sos = 0.0;
+		for (int i = 0; i < sequences.length; i++) {
+			double dist = DTW(sequence,sequences[i]);
+			sos += dist*dist;
+		}
+		return sos;
+	}
+	
+	public static double DTW(double[]S,double []T) {
+		int i, j;
+		costMatrix[0][0] = squaredDistance(S[0],T[0]);
+		for (i = 1; i < S.length; i++) {
+			costMatrix[i][0] = costMatrix[i - 1][0] + squaredDistance(S[i], T[0]);
+		}
+		for (j = 1; j < T.length; j++) {
+			costMatrix[0][j] = costMatrix[0][j - 1]	+ squaredDistance(S[0],T[j]);
+		}
+		for (i = 1; i < S.length; i++) {
+			for (j = 1; j < T.length; j++) {
+				costMatrix[i][j] = Min3(costMatrix[i - 1][j - 1],
+						costMatrix[i][j - 1], costMatrix[i - 1][j])
+						+ squaredDistance(S[i],T[j]);
+			}
+		}
+		
+		return sqrt(costMatrix[S.length - 1][T.length - 1]);
+	}
+	
+	public static double[] DBAUpdate(double[] C, double[][] sequences) {
+		double[]updatedMean = new double[C.length];
+		int[]nElementsForMean= new int[C.length];
+		
+		int i, j, indiceRes;
 		double res = 0.0;
 		int centerLength = C.length;
 		int seqLength;
@@ -68,19 +135,16 @@ public class DBA {
 		for (double[] T : sequences) {
 			seqLength = T.length;
 
-			costMatrix[0][0] = distanceTo(C[0], T[0]);
+			costMatrix[0][0] = squaredDistance(C[0], T[0]);
 			pathMatrix[0][0] = DBA.NIL;
-			optimalPathLength[0][0] = 0;
 
 			for (i = 1; i < centerLength; i++) {
-				costMatrix[i][0] = costMatrix[i - 1][0] + distanceTo(C[i], T[0]);
+				costMatrix[i][0] = costMatrix[i - 1][0] + squaredDistance(C[i], T[0]);
 				pathMatrix[i][0] = DBA.UP;
-				optimalPathLength[i][0] = i;
 			}
 			for (j = 1; j < seqLength; j++) {
-				costMatrix[0][j] = costMatrix[0][j - 1] + distanceTo(T[j], C[0]);
+				costMatrix[0][j] = costMatrix[0][j - 1] + squaredDistance(T[j], C[0]);
 				pathMatrix[0][j] = DBA.LEFT;
-				optimalPathLength[0][j] = j;
 			}
 
 			for (i = 1; i < centerLength; i++) {
@@ -90,28 +154,24 @@ public class DBA {
 					switch (indiceRes) {
 						case DIAGONAL:
 							res = costMatrix[i - 1][j - 1];
-							optimalPathLength[i][j] = optimalPathLength[i - 1][j - 1] + 1;
 							break;
 						case LEFT:
 							res = costMatrix[i][j - 1];
-							optimalPathLength[i][j] = optimalPathLength[i][j - 1] + 1;
 							break;
 						case UP:
 							res = costMatrix[i - 1][j];
-							optimalPathLength[i][j] = optimalPathLength[i - 1][j] + 1;
 							break;
 					}
-					costMatrix[i][j] = res + distanceTo(C[i], T[j]);
+					costMatrix[i][j] = res + squaredDistance(C[i], T[j]);
 				}
 			}
-
-			nbTuplesAverageSeq = optimalPathLength[centerLength - 1][seqLength - 1] + 1;
 
 			i = centerLength - 1;
 			j = seqLength - 1;
 
-			for (int t = nbTuplesAverageSeq - 1; t >= 0; t--) {
-				tupleAssociation[i].add(T[j]);
+			while(pathMatrix[i][j]!=DBA.NIL) {
+				updatedMean[i]+=T[j];
+				nElementsForMean[i]++;
 				switch (pathMatrix[i][j]) {
 					case DIAGONAL:
 						i = i - 1;
@@ -124,17 +184,20 @@ public class DBA {
 						i = i - 1;
 						break;
 				}
-
 			}
-
+			assert(i!=0 || j!=0);
+			updatedMean[i]+=T[j];
+			nElementsForMean[i]++;
 		}
 
 		for (int t = 0; t < centerLength; t++) {
-
-			C[t] = barycenter((tupleAssociation[t].toArray()));
+			updatedMean[t] /= nElementsForMean[t];
 		}
+		
+		return updatedMean;
 
 	}
+
 
 
 	public static double Min3(final double a, final double b, final double c) {
@@ -169,7 +232,7 @@ public class DBA {
 		}
 	}
 
-	public static double distanceTo(double a, double b) {
+	public static double squaredDistance(double a, double b) {
 		return (a - b) * (a - b);
 	}
 
@@ -187,34 +250,15 @@ public class DBA {
 	}
 
 	public static void main(String [] args){
-		double [][]sequences = new double[100][];
+		Random r = new Random(3071980);
+		double [][]sequences = new double[50][];
 		for(int i=0;i<sequences.length;i++){
 			sequences[i] = new double[20];
 			for(int j=0;j<sequences[i].length;j++){
-				sequences[i][j] = Math.cos(Math.random()*j/20.0*Math.PI) ;
+				sequences[i][j] = Math.cos(r.nextDouble()*j/20.0*Math.PI) ;
 			}
 		}
-		double [] averageSequence = new double[20];
-		int choice = (int) Math.random()*100;
-		for(int j=0;j<averageSequence.length;j++){
-			averageSequence[j] = sequences[choice][j] ;
-		}
-		
-		System.out.print("[");
-		for(int j=0;j<averageSequence.length;j++){
-			System.out.print(averageSequence[j]+" ");
-		}
-		System.out.println("]");
-		
-		DBA(averageSequence, sequences);
-		
-		System.out.print("[");
-		for(int j=0;j<averageSequence.length;j++){
-			System.out.print(averageSequence[j]+" ");
-		}
-		System.out.println("]");
-		
-		DBA(averageSequence, sequences);
+		double [] averageSequence = performDBA(sequences);
 		
 		System.out.print("[");
 		for(int j=0;j<averageSequence.length;j++){
