281bcf26b9b6d4ced77b2b8149cd88ed247e5508
Francois Petitjean <francois.petitjean@monash.edu>
2018-05-04 16:47:27 +0200

    optimization of DBA with warping window

--- DBAWarpingWindow.java
+++ DBAWarpingWindow.java
@@ -14,7 +14,12 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  ******************************************************************************/ 
 
+import static java.lang.Math.sqrt;
+
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Random;
 
 /**
  * This toy class show the use of DBA with warping window.	
@@ -44,9 +49,88 @@ public class DBAWarpingWindow {
 	private static int[][] pathMatrix = new int[MAX_SEQ_LENGTH][MAX_SEQ_LENGTH];
 
 	/**
-	 * Store the length of the optimal path in each cell
+	 * Performs the DBA averaging by first finding the median over a sample, then doing n iterations of the update
+	 * @param sequences set of sequences to average
+	 * @param w warping window
 	 */
-	private static int[][] optimalPathLength = new int[MAX_SEQ_LENGTH][MAX_SEQ_LENGTH];
+	public static double[] performDBA(double[][] sequences,int w) {
+		int medoidIndex = approximateMedoidIndex(sequences,w);
+		double[]center = Arrays.copyOf(sequences[medoidIndex], sequences[medoidIndex].length);
+		
+		for (int i = 0; i < 15; i++) {
+			center = DBAUpdate(center, sequences,w);
+		}
+		return center;
+	}
+	
+	public static int approximateMedoidIndex(double[][] sequences,int w) {
+		/*
+		 * we are finding the medoid, as this can take a bit of time, 
+		 * if there is more than 50 time series, we sample 50 as possible 
+		 * medoid candidates
+		 */
+		ArrayList<Integer>allIndices = new ArrayList<>();
+		for (int i = 0; i < sequences.length; i++) {
+			allIndices.add(i);
+		}
+		Collections.shuffle(allIndices);
+		ArrayList<Integer>medianIndices = new ArrayList<>();
+		for (int i = 0; i < sequences.length && i<50; i++) {
+			medianIndices.add(allIndices.get(i));
+		}
+		
+		int indexMedoid = -1;
+		double lowestSoS = Double.MAX_VALUE;
+		
+		for (int medianCandidateIndex:medianIndices) {
+			double[] possibleMedoid = sequences[medianCandidateIndex];
+			double tmpSoS = sumOfSquares(possibleMedoid, sequences,w);
+			if (tmpSoS < lowestSoS) {
+				indexMedoid = medianCandidateIndex;
+				lowestSoS = tmpSoS;
+			}
+		}
+		return indexMedoid;
+	}
+	
+	private static double sumOfSquares(double[]sequence,double[][]sequences, int w) {
+		double sos = 0.0;
+		for (int i = 0; i < sequences.length; i++) {
+			double dist = DTW(sequence,sequences[i],w);
+			sos += dist*dist;
+		}
+		return sos;
+	}
+	
+	public static double DTW(double[]S,double []T, int w) {
+		int i, j;
+		costMatrix[0][0] = squaredDistance(S[0],T[0]);
+		for (i = 1; i < Math.min(S.length,w+1); i++) {
+			costMatrix[i][0] = costMatrix[i - 1][0] + squaredDistance(S[i], T[0]);
+		}
+		for (j = 1; j < Math.min(T.length,w+1); j++) {
+			costMatrix[0][j] = costMatrix[0][j - 1]	+ squaredDistance(S[0],T[j]);
+		}
+		if (j < T.length)
+			costMatrix[0][j] = Double.POSITIVE_INFINITY;
+		
+		for (i = 1; i < S.length; i++) {
+			int jStart = Math.max(1, i-w);
+			int jStop = Math.min(T.length, i+w+1);
+			int indexInftyLeft = i-w-1;
+			if(indexInftyLeft>=0)costMatrix[i][indexInftyLeft] = Double.POSITIVE_INFINITY;
+			for (j = jStart; j < jStop; j++) {
+				costMatrix[i][j] = Min3(costMatrix[i - 1][j - 1],
+						costMatrix[i][j - 1], costMatrix[i - 1][j])
+						+ squaredDistance(S[i],T[j]);
+			}
+			if (jStop < T.length)
+				costMatrix[i][jStop] = Double.POSITIVE_INFINITY;
+		}
+		
+		return sqrt(costMatrix[S.length - 1][T.length - 1]);
+	}
+	
 
 	/**
 	 * Dtw Barycenter Averaging (DBA)
@@ -54,12 +138,11 @@ public class DBAWarpingWindow {
 	 * @param sequences set of sequences to average
 	 * @param w warping window size for DTW
 	 */
-	public static double[] DBA_update(double[] C, double[][] sequences, int w) {
-		final ArrayList<Double>[] tupleAssociation = new ArrayList[C.length];
-		for (int i = 0; i < tupleAssociation.length; i++) {
-			tupleAssociation[i] = new ArrayList<Double>(sequences.length);
-		}
-		int nbTuplesAverageSeq, i, j, indiceRes;
+	public static synchronized double[] DBAUpdate(double[] C, double[][] sequences, int w) {
+		double[]updatedMean = new double[C.length];
+		int[]nElementsForMean= new int[C.length];
+		
+		int i, j, indiceRes;
 		double res = 0.0;
 		int centerLength = C.length;
 		int seqLength, jStart, jStop;;
@@ -67,19 +150,16 @@ public class DBAWarpingWindow {
 		for (double[] T : sequences) {
 			seqLength = T.length;
 
-			costMatrix[0][0] = distanceTo(C[0], T[0]);
+			costMatrix[0][0] = squaredDistance(C[0], T[0]);
 			pathMatrix[0][0] = NIL;
-			optimalPathLength[0][0] = 0;
 
 			for (i = 1; i < Math.min(centerLength, 1+w); i++) {
-				costMatrix[i][0] = costMatrix[i - 1][0] + distanceTo(C[i], T[0]);
+				costMatrix[i][0] = costMatrix[i - 1][0] + squaredDistance(C[i], T[0]);
 				pathMatrix[i][0] = UP;
-				optimalPathLength[i][0] = i;
 			}
 			for (j = 1; j < Math.min(seqLength, 1+w); j++) {
-				costMatrix[0][j] = costMatrix[0][j - 1] + distanceTo(T[j], C[0]);
+				costMatrix[0][j] = costMatrix[0][j - 1] + squaredDistance(T[j], C[0]);
 				pathMatrix[0][j] = LEFT;
-				optimalPathLength[0][j] = j;
 			}
 			if (j < seqLength)
 				costMatrix[0][j] = Double.POSITIVE_INFINITY;
@@ -95,30 +175,26 @@ public class DBAWarpingWindow {
 					switch (indiceRes) {
 						case DIAGONAL:
 							res = costMatrix[i - 1][j - 1];
-							optimalPathLength[i][j] = optimalPathLength[i - 1][j - 1] + 1;
 							break;
 						case LEFT:
 							res = costMatrix[i][j - 1];
-							optimalPathLength[i][j] = optimalPathLength[i][j - 1] + 1;
 							break;
 						case UP:
 							res = costMatrix[i - 1][j];
-							optimalPathLength[i][j] = optimalPathLength[i - 1][j] + 1;
 							break;
 					}
-					costMatrix[i][j] = res + distanceTo(C[i], T[j]);
+					costMatrix[i][j] = res + squaredDistance(C[i], T[j]);
 				}
 				if (jStop < seqLength)
 					costMatrix[i][jStop] = Double.POSITIVE_INFINITY;
 			}
 
-			nbTuplesAverageSeq = optimalPathLength[centerLength-1][seqLength-1] + 1;
-
 			i = centerLength - 1;
 			j = seqLength - 1;
 			
-			for (int t = nbTuplesAverageSeq - 1; t >= 0; t--) {
-				tupleAssociation[i].add(T[j]);
+			while(pathMatrix[i][j]!=NIL) {
+				updatedMean[i]+=T[j];
+				nElementsForMean[i]++;
 				switch (pathMatrix[i][j]) {
 					case DIAGONAL:
 						i = i - 1;
@@ -132,12 +208,17 @@ public class DBAWarpingWindow {
 						break;
 				}
 			}
+			if(i!=0 ||j!=0)throw new RuntimeException("This should never occur, please contact Francois <francois.petitjean@monash.edu>");
+			//i and j should be 0
+			updatedMean[i]+=T[j];
+			nElementsForMean[i]++;
 		}
 		
 		for (int t = 0; t < centerLength; t++) {
-			C[t] = barycenter((tupleAssociation[t].toArray()));
+			updatedMean[t] /= nElementsForMean[t];
 		}
-		return C;
+		
+		return updatedMean;
 	}
 
 
@@ -173,7 +254,7 @@ public class DBAWarpingWindow {
 		}
 	}
 
-	public static double distanceTo(double a, double b) {
+	public static double squaredDistance(double a, double b) {
 		return (a - b) * (a - b);
 	}
 
@@ -191,34 +272,21 @@ public class DBAWarpingWindow {
 	}
 
 	public static void main(String [] args){
-		int w = 1;
-		double [][]sequences = new double[100][];
+		int w = 5;
+		Random r = new Random(3071980);
+		double [][]sequences = new double[50][];
 		for(int i=0;i<sequences.length;i++){
 			sequences[i] = new double[20];
 			for(int j=0;j<sequences[i].length;j++){
-				sequences[i][j] = Math.cos(Math.random()*j/20.0*Math.PI) ;
+				sequences[i][j] = Math.cos(r.nextDouble()*j/20.0*Math.PI) ;
 			}
 		}
-		double [] averageSequence = new double[20];
-		int choice = (int) Math.random()*100;
-		for(int j=0;j<averageSequence.length;j++){
-			averageSequence[j] = sequences[choice][j] ;
-		}
+		double [] averageSequence = performDBA(sequences,w);
 		
 		System.out.print("[");
 		for(int j=0;j<averageSequence.length;j++){
 			System.out.print(averageSequence[j]+" ");
 		}
 		System.out.println("]");
-		
-		for (int i=0; i<10; i++){
-			DBA_update(averageSequence, sequences, w);
-			
-			System.out.print("[");
-			for(int j=0;j<averageSequence.length;j++){
-				System.out.print(averageSequence[j]+" ");
-			}
-			System.out.println("]");
-		}
 	}
 }
