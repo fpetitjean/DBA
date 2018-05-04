bec0f1e7e699783ae509a14398706a439cbbcb9b
Francois Petitjean <francois.petitjean@monash.edu>
2018-05-04 17:03:55 +0200

    adding F to (c)

--- DBAWarpingWindowMultiThreadCapable.java
+++ DBAWarpingWindowMultiThreadCapable.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (C) 2016 Chang Wei Tan 
+ * Copyright (C) 2018 Chang Wei Tan, François Petitjean 
  * 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
