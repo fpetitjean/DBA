73d778989d05bed00f1f4fa24b116caea78be463
Francois Petitjean <francois.petitjean@monash.edu>
2019-01-18 07:57:06 +1100

    cleanup

--- cython/setup.py
+++ cython/setup.py
@@ -3,7 +3,6 @@ from Cython.Build import cythonize
 import numpy
 
 setup(
-    # ext_modules=cythonize("shape_DBA.pyx", compiler_directives={'profile': True}),
     ext_modules=cythonize("DBA.pyx",compiler_directives={'boundscheck':False,'wraparound':False}),
     include_dirs=[numpy.get_include()]
 )
